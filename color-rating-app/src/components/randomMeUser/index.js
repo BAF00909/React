03037a34f8c20c4f8762c550a57b19073f61bece
Alexander Breus <lis00909@gmail.com>
2018-04-29 21:17:15 +0300

    edit projects

--- color-rating-app/src/components/randomMeUser/index.js
+++
@@ -1,4 +0,0 @@
-import PeopleListFun from '../peopleListFun';
-import DataComponent from '../dataComponent';
-
-export const RandomMeUser = DataComponent(PeopleListFun,"https://randomuser.me/api/");
